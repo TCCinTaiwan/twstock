76adaae6abc34719ca4429d821b4a3cdd0f11178
TCC <john987john987@gmail.com>
2017-10-17 14:50:03 +0800

    Fix spelling errors

--- twstock/__init__.py
+++ twstock/__init__.py
@@ -6,7 +6,7 @@ from twstock import cli
 from twstock import mock
 from twstock import realtime
 
-from twstock.analytics import BestFourPoint, StochastiOscillator
+from twstock.analytics import BestFourPoint, StochasticOscillator
 from twstock.codes import twse, tpex, codes
 from twstock.stock import Stock
 
